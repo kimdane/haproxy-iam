edd826cb189c0e107ccf0c9d4e999387b9618e37
Kim Daniel Engebretsen <kim@conduct.no>
2017-06-15 22:03:32 -0400

    Update run.sh
    refactoring into run.sh

--- run.sh
+++ run.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 
-if [ ! -d /etc/ssl ] ;then 
-	mkdir -p /etc/ssl 
-fi
+if [ ! -d /etc/ssl ]; then mkdir -p /etc/ssl; fi
 cd /etc/ssl
-if [ -f /opt/repo/tls/combined.pem ]; then
-	cp /opt/repo/tls/combined.pem /etc/ssl/combined.pem
+if [ -f /opt/repo/ssl/combined.pem ]; then
+	cp /opt/repo/ssl/combined.pem /etc/ssl/combined.pem
+	sed -i 's/iam.example.com/'$(openssl x509 -noout -subject -in /etc/ssl/combined.pem | sed "s/^.*CN=\*\./iam./" | sed "s/^.*CN=//" | sed "s/\/.*$//")'/' /usr/local/etc/haproxy/haproxy.cfg
 else
 	openssl req -x509 -newkey rsa:2048 -keyout combined.pem -out combined.pem -days 999 -nodes -subj "/CN=iam.example.com"	
 fi
-sed -i 's/iam.example.com/'$(openssl x509 -noout -subject -in combined.pem | sed "s/^.*CN=//" | sed "s/\/.*$//")'/' /usr/local/etc/haproxy/haproxy.cfg
 
 haproxy -db -f /usr/local/etc/haproxy/haproxy.cfg
