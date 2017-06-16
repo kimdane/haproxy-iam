fb1751a4840c4d3cf4999cc967a6cadc895891e6
Kim Daniel Engebretsen <kim@conduct.no>
2017-06-15 22:24:08 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-if [ ! -d /etc/ssl ]; then mkdir -p /etc/ssl; fi
-cd /etc/ssl
-if [ -f /opt/repo/ssl/combined.pem ]; then
-	cp /opt/repo/ssl/combined.pem /etc/ssl/combined.pem
+if [ -f /etc/ssl/combined.pem ]; then
 	sed -i 's/iam.example.com/'$(openssl x509 -noout -subject -in /etc/ssl/combined.pem | sed "s/^.*CN=\*\./iam./" | sed "s/^.*CN=//" | sed "s/\/.*$//")'/' /usr/local/etc/haproxy/haproxy.cfg
 else
+	mkdir -p /etc/ssl
+	cd /etc/ssl
 	openssl req -x509 -newkey rsa:2048 -keyout combined.pem -out combined.pem -days 999 -nodes -subj "/CN=iam.example.com"	
 fi
 
