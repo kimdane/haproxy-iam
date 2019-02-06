6b54797acfb97d6c3e2f244363fd656b3a4bf7b8
Kim Daniel Engebretsen <kim@conduct.no>
2019-02-06 03:42:53 -0500

    forgot to fill sed command

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 if [ -f /etc/ssl/combined.pem ]; then
 	FQDN=$(openssl x509 -noout -subject -in /etc/ssl/combined.pem | sed "s/^.*CN=\*\./iam./" | sed "s/^.*CN=//" | sed "s/\/.*$//")
-	sed -i 's/iam.example.com//g' /usr/local/etc/haproxy/haproxy.cfg
+	sed -i 's/iam.example.com/$FQDN/g' /usr/local/etc/haproxy/haproxy.cfg
 else
 	mkdir -p /etc/ssl
 	cd /etc/ssl
